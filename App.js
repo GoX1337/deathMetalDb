9ad434116efe785c515148825a9c6ded18f447b1
GoX1337 <gox.roche@gmail.com>
2017-10-06 02:06:39 +0200

    Add search input

--- App.js
+++ App.js
@@ -1,14 +1,32 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, ScrollView } from 'react-native';
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { text: '' };
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <ScrollView contentContainerStyle={styles.container}
+                  scrollEnabled={false}>
+                  
+          <Text style={styles.title} >Search a death metal band</Text>
+
+          <TextInput style={styles.input} 
+                            onChangeText={(text) => this.setState({text})} 
+                            value={this.state.text} 
+                            placeholder="Band name, country, city..." 
+                            autoCorrect={false}
+                            underlineColorAndroid="transparent" 
+                            keyboardType={"web-search"}
+                            inlineImageLeft='search_icon'
+                            returnKeyLabel={"search"}
+                            />
+
+      </ScrollView>
     );
   }
 }
@@ -20,4 +38,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontSize: 25,
+    marginBottom : 15
+  },
+  input: {
+    height: 40, 
+    width: 250,
+    borderColor: 'gray',
+    borderWidth: 1,
+    paddingLeft: 10,
+    fontSize: 18
+  }
 });
