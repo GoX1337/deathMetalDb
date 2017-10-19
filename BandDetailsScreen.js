042dee8da2b6fe6d701566a748d0ece7b59af546
GoX1337 <gox.roche@gmail.com>
2017-10-19 18:57:32 +0200

    Add band info and images

--- BandDetailsScreen.js
+++ BandDetailsScreen.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import { Container, Content} from 'native-base';
+import { View, Text, Image } from 'react-native';
+import { Container, Header, Title, Content, Button, Left, Right, Body, Icon, Item, Input, List, ListItem, Thumbnail} from 'native-base';
 import styles from './Styles';
 
 class BandDetailsScreen extends React.Component {
     
-    constructor(props) {
-        super(props);
-    }
-
     static navigationOptions = ({ navigation }) => ({
         title: `${navigation.state.params.band.name}`,
         modal: true,
@@ -15,14 +12,64 @@ class BandDetailsScreen extends React.Component {
             textAlign: 'center'
         },
         headerStyle: {
-            backgroundColor:'white',
             height: 85
         }
     });
 
+    constructor(props) {
+        super(props);
+        const b = this.props.navigation.state.params.band;
+        const labels = this.buildLabels();
+        this.state = { 
+            band: b,
+            bandInfo : this.buildBandInfo(b, labels)
+        };
+    }
+
+    buildLabels(){
+        let labels = {};
+        labels["trans"] = {
+            "active":"Status",
+            "yearActive":"Year active",
+            "genre":"Genre",
+            "country":"Country",
+            "location":"Location",
+            "formedIn":"Formation date",
+            "lyricsTheme":"Lyrics theme"
+        };     
+        labels["order"] = ["genre", "active","country","location","formedIn","yearActive","lyricsTheme"];
+        return labels;
+    }
+
+    buildBandInfo(band, labels){
+        let data = [];
+        labels.order.forEach((key)=>{
+            let row = {};
+            row.label = labels.trans[key];
+            row.val = band[key];
+            data.push(row);
+        });
+        return data;
+    }
+
     render(){
         return (
-            <Container></Container>
+            <Container>
+                <Content>
+                    <Image resizeMode={'contain'} style={{width: 250, height: 150}} source={{uri: this.state.band.logo}}/>
+                    <List dataArray={this.state.bandInfo}
+                        renderRow={(rowData) =>
+                        <ListItem>
+                            <Left><Text>{rowData.label}</Text></Left>
+                            <Body>
+                            <Text>{rowData.val}</Text>
+                            </Body>
+                        </ListItem>
+                        }>
+                    </List>
+                    <Image resizeMode={'contain'} style={{width: 300, height:200}} source={{uri: this.state.band.photo}}/>
+                </Content>
+            </Container>
         );
     }
 };
